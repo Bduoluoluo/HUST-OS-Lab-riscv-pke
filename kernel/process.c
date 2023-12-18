8006f81bd3f4d0b93582834a86bdae0f10ae179b
root <root@localhost.localdomain>
2023-12-19 00:03:33 +0800

    lab3_1

--- kernel/process.c
+++ kernel/process.c
@@ -179,7 +179,7 @@ int do_fork( process* parent)
 {
   sprint( "will fork a child from parent %d.\n", parent->pid );
   process* child = alloc_process();
-
+  int free_block_filter[MAX_HEAP_PAGES];
   for( int i=0; i<parent->total_mapped_region; i++ ){
     // browse parent's vm space, and copy its trapframe and data segments,
     // map its code segment.
@@ -196,7 +196,7 @@ int do_fork( process* parent)
 
         // convert free_pages_address into a filter to skip reclaimed blocks in the heap
         // when mapping the heap blocks
-        int free_block_filter[MAX_HEAP_PAGES];
+        
         memset(free_block_filter, 0, MAX_HEAP_PAGES);
         uint64 heap_bottom = parent->user_heap.heap_bottom;
         for (int i = 0; i < parent->user_heap.free_pages_count; i++) {
@@ -233,7 +233,6 @@ int do_fork( process* parent)
         // DO NOT COPY THE PHYSICAL PAGES, JUST MAP THEM.
         // panic( "You need to implement the code segment mapping of child in lab3_1.\n" );
         map_pages((pagetable_t)child->pagetable, parent->mapped_info[i].va, PGSIZE * parent->mapped_info[i].npages, lookup_pa((pagetable_t)parent->pagetable, parent->mapped_info[i].va), prot_to_type(PROT_EXEC | PROT_READ, 1));
-        // sprint("do_fork map code segment at pa:%lx of parent to child at va:%lx.\n", lookup_pa((pagetable_t)parent->pagetable, parent->mapped_info[i].va), parent->mapped_info[i].va);
 
         // after mapping, register the vm region (do not delete codes below!)
         child->mapped_info[child->total_mapped_region].va = parent->mapped_info[i].va;
